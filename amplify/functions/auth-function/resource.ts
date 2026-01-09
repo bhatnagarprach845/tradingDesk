faf1d7c5556cbd6d91614ebc8be438fd496682ec
Anil <anil.s.sv@gmail.com>
2026-01-09 11:09:48 -0500

    updated pythin version

--- amplify/functions/auth-function/resource.ts
+++ amplify/functions/auth-function/resource.ts
@@ -3,7 +3,6 @@ import { defineFunction } from "@aws-amplify/backend";
 export const authFunction = defineFunction({
   name: "auth-function",
   entry: "./handler.py",
-  // Some versions of the CLI prefer the full string identifier
-  runtime: 3.9,
+  runtime: "python3.9",
   timeoutSeconds: 20,
 });
