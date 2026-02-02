dc443975ca927e15f2a3fc4bfc037d1b735d1008
Anil <anil.s.sv@gmail.com>
2026-02-02 11:30:56 -0500

    4

--- amplify/functions/python-upload/resource.ts
+++ amplify/functions/python-upload/resource.ts
@@ -1,15 +1,17 @@
 // amplify/functions/python-upload/resource.ts
 import { defineFunction } from "@aws-amplify/backend";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { Runtime, Function, Code } from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
 
-export const pythonUpload = defineFunction((scope) => {
-  const { Function, Code } = require("aws-cdk-lib/aws-lambda");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+export const pythonUpload = defineFunction((scope) => {
   return new Function(scope, "python-upload-lambda", {
     handler: "handler.handler", // filename.function_name
     runtime: Runtime.PYTHON_3_11,
-    code: Code.fromAsset(path.dirname(__filename)), // Points to this folder
+    // Points to the current directory containing handler.py and fifo_engine.py
+    code: Code.fromAsset(__dirname),
     timeoutSeconds: 30, // CSV processing can take time
   });
 });
