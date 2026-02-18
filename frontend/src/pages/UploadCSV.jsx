3dec54afc6705b48c668cf30ea3eae3666ac5764
Anil <anil.s.sv@gmail.com>
2026-02-18 10:41:48 -0500

    Close to Final1

--- frontend/src/pages/UploadCSV.jsx
+++ frontend/src/pages/UploadCSV.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import {
   Box, Button, Typography, Container, Grid, Paper, CircularProgress,
   FormControl, InputLabel, Select, MenuItem, createTheme, ThemeProvider,
